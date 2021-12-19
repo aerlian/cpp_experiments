e48ea79c2fe43350bc2ba5d3e603e521f1344ab9
Steve <steve.rodgers@gmail.com>
2021-12-19 11:52:01 -0500

    add move collateral

---
+++ smart_pointer/move/move_example.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Stephen Rodgers on 12/19/21.
+//
+
+#include "move_example.h"
---
+++ smart_pointer/move/move_example.h
@@ -0,0 +1,14 @@
+//
+// Created by Stephen Rodgers on 12/19/21.
+//
+
+#ifndef UNTITLED_MOVE_EXAMPLE_H
+#define UNTITLED_MOVE_EXAMPLE_H
+
+
+class move_example {
+
+};
+
+
+#endif //UNTITLED_MOVE_EXAMPLE_H
